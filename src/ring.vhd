b39d195d3747d8400eb50b2f3e238dd876e954c0
Anders Struck <s143217@student.dtu.dk>
2020-05-04 15:18:54 +0200

    Updated Ring oscillator with a generator based architecture

--- src/ring.vhd
+++ src/ring.vhd
@@ -14,6 +14,7 @@
 -- Revision:
 -- Revision 0.01 - 21.04.2020 - File Created
 -- Revision 0.10 - 21.04.2020 - First implementation
+-- Revision 0.20 - 04.05.2020 - Changed to for-generate method for generating the delay LUTS
 -- 
 ----------------------------------------------------------------------------------
 -- The Unisim Library is used to define Xilinx primitives. It is also used during
@@ -21,181 +22,46 @@
 --
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.numeric_std.all;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 library unisim;
 use unisim.vcomponents.all;
---
-------------------------------------------------------------------------------------
---
--- Main Entity for ring_osc
---
+
 entity ring_osc is
-  port(   ro_out : out std_logic;
-            reset : in std_logic   );
-  end ring_osc;
---
-------------------------------------------------------------------------------------
---
--- Start of Main Architecture for ring_osc
---     
+	generic (
+   			LUTS 	: Natural := 10);		-- neutral = 0 to Integer'high
+  	port(   ro_out 	: out std_logic;
+            reset 	: in std_logic   );
+end ring_osc;
+
 architecture low_level_definition of ring_osc is
---
-------------------------------------------------------------------------------------
---
--- Signals used in RING_OSC
---
-------------------------------------------------------------------------------------
---
-signal ring_delay1      : std_logic;
-signal ring_delay2      : std_logic;
-signal ring_delay3      : std_logic;
-signal ring_delay4      : std_logic;
-signal ring_delay5      : std_logic;
-signal ring_delay6      : std_logic;
-signal ring_delay7      : std_logic;
-signal ring_delay8      : std_logic;
-signal ring_delay9      : std_logic;
-signal ring_delay10      : std_logic;
-signal ring_delay11      : std_logic;
-signal ring_delay12      : std_logic;
-signal ring_invert      : std_logic;
-signal toggle           : std_logic;
-signal clk_div2         : std_logic;
+	signal ring_delay	: std_logic_vector(LUTS downto 0);
+	signal resetn 		: std_logic;
+	
+	-- The following constants are defined to allow for
+	--   equation-based INIT specification for a LUT2.
+	constant I0 : BIT_VECTOR(3 downto 0) := X"A";
+	constant I1 : BIT_VECTOR(3 downto 0) := X"C";
+	
+	-- Attributes to stop delay logic from being optimised.
+	attribute KEEP : string; 
+	attribute KEEP of ring_delay 		: signal is "true";
+	attribute DONT_TOUCH : string;
+	attribute DONT_TOUCH of ring_delay 	: signal is "true";
 
-signal resetn : std_logic;
-
--- The following constants are defined to allow for
---   equation-based INIT specification for a LUT2.
-constant I0 : BIT_VECTOR(3 downto 0) := X"A";
-constant I1 : BIT_VECTOR(3 downto 0) := X"C";
-
---
--- Attributes to stop delay logic from being optimised.
---
-attribute KEEP : string; 
-attribute KEEP of ring_delay1 		: signal is "true"; 
-attribute KEEP of ring_delay2 		: signal is "true"; 
-attribute KEEP of ring_delay3 		: signal is "true"; 
-attribute KEEP of ring_delay4 		: signal is "true"; 
-attribute KEEP of ring_delay5 		: signal is "true"; 
-attribute KEEP of ring_delay6 		: signal is "true";
-attribute KEEP of ring_delay7 		: signal is "true"; 
-attribute KEEP of ring_delay8 		: signal is "true"; 
-attribute KEEP of ring_delay9 		: signal is "true"; 
-attribute KEEP of ring_delay10 		: signal is "true"; 
-attribute KEEP of ring_delay11		: signal is "true"; 
-attribute KEEP of ring_delay12 		: signal is "true";
-attribute KEEP of ring_invert 		: signal is "true";
---
--- Attributes to stop delay logic from being optimised.
---
-attribute DONT_TOUCH : string;
-attribute DONT_TOUCH of ring_delay1 	: signal is "true";
-attribute DONT_TOUCH of ring_delay2 	: signal is "true";
-attribute DONT_TOUCH of ring_delay3 	: signal is "true";
-attribute DONT_TOUCH of ring_delay4 	: signal is "true";
-attribute DONT_TOUCH of ring_delay5 	: signal is "true";
-attribute DONT_TOUCH of ring_delay6 	: signal is "true";
-attribute DONT_TOUCH of ring_invert 	: signal is "true";
-attribute DONT_TOUCH of ring_delay7 	: signal is "true"; 
-attribute DONT_TOUCH of ring_delay8 	: signal is "true"; 
-attribute DONT_TOUCH of ring_delay9 	: signal is "true"; 
-attribute DONT_TOUCH of ring_delay10 	: signal is "true"; 
-attribute DONT_TOUCH of ring_delay11	: signal is "true"; 
-attribute DONT_TOUCH of ring_delay12 	: signal is "true";
---
-------------------------------------------------------------------------------------
---    
--- Circuit description
---
-------------------------------------------------------------------------------------
---    
 begin
-	ro_out <= ring_invert;
 	resetn <= NOT reset;
-
-	--
-	--Ring oscillator is formed of 5 levels of logic of which one is an inverter.
-	--
-	
-	delay1_lut: LUT2 generic map (
+	-- The Ring oscilator is generated from the generic LUTS
+	delay_generator:
+   	for i in 0 to LUTS-1 generate
+      	begin
+        delay: LUT2 generic map (
 		INIT => I1 AND I0)
 		port map( 	I0 	=> resetn,
-					I1 	=> ring_invert,
-			 		O 	=> ring_delay1 );
-	
-	delay2_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay1,
-			 		O 	=> ring_delay2 );
-	
-	delay3_lut: LUT2 generic map (
-		INIT => I1 AND I0) 
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay2,
-			 		O 	=> ring_delay3 );
-	
-	delay4_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1	=> ring_delay3,
-				 	O 	=> ring_delay4 );
-	
-	
-	delay5_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay4,
-			 		O 	=> ring_delay5 );
-			 
-	delay6_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay5,
-					O 	=> ring_delay6 );
-
-
-
-
-	delay7_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay6,
-			 		O 	=> ring_delay7 );
-	
-	delay8_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay7,
-			 		O 	=> ring_delay8 );
-	
-	delay9_lut: LUT2 generic map (
-		INIT => I1 AND I0) 
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay8,
-			 		O 	=> ring_delay9 );
-	
-	delay10_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1	=> ring_delay9,
-				 	O 	=> ring_delay10 );
-	
-	
-	delay11_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay10,
-			 		O 	=> ring_delay11 );
-			 
-	delay12_lut: LUT2 generic map (
-		INIT => I1 AND I0)
-		port map( 	I0 	=> resetn,
-					I1 	=> ring_delay11,
-					O 	=> ring_delay12 );
-	
-	ring_invert <= not ring_delay12;
-	
+					I1 	=> ring_delay(i),
+			 		O 	=> ring_delay(i+1) );
+   	end generate;
+   	-- A not component is used as the last element in the ring
+	ring_delay(0) <= not ring_delay(LUTS);
+	ro_out <= ring_delay(0);
 end low_level_definition;
--- src/top.vhd
+++ src/top.vhd
@@ -14,6 +14,7 @@
 -- Revision:
 -- Revision 0.01 - 21.04.2020 - File Created
 -- Revision 0.10 - 21.04.2020 - First implementation
+-- Revision 0.20 - 04.05.2020 - Added With Ring Oscilator
 -- 
 ----------------------------------------------------------------------------------
 
@@ -22,44 +23,46 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
-    Port ( CLK100MHZ    : in STD_LOGIC;
+    Port ( CLK100MHZ: in STD_LOGIC;
            reset    : in STD_LOGIC;
-           r_osc    : out STD_LOGIC;
-           LED        : out STD_LOGIC_VECTOR (0 to 9));
+           r_osc	: out STD_LOGIC;
+           LED      : out STD_LOGIC_VECTOR (0 to 9));
 end top;
 
 architecture Behavioral of top is
-  signal Ring_oscilator    : STD_LOGIC;
-  signal count_enable, clk : STD_LOGIC;
+  	signal Ring_oscilator    : STD_LOGIC;
+  	signal count_enable, clk : STD_LOGIC;
 
-  component ring_osc is
-    port (  reset   : in std_logic;
-            ro_out  : out std_logic);
-  end component;
-  component clk_wiz_0 is
+  	component ring_osc is
+  		generic (
+   				LUTS 	: Natural := 4);
+   		port (  reset   : in std_logic;
+            	ro_out  : out std_logic);
+  	end component;
+  	component clk_wiz_0 is
         Port (  clk_in1 : in STD_LOGIC;
                 reset   : in STD_LOGIC;
                 clk_out : out STD_LOGIC;
                 locked  : out STD_LOGIC);
     end component;
     component dut is
-        Port (  clk   : in STD_LOGIC;
+        Port (  clk   	: in STD_LOGIC;
                 reset   : in STD_LOGIC;
-                data  : in STD_LOGIC;
+                data  	: in STD_LOGIC;
                 count_en: out STD_LOGIC);
     end component;
     component counter is
-    Port (  clk   : in STD_LOGIC;
-            reset   : in STD_LOGIC;
-            EN      : in STD_LOGIC;
-            count   : out STD_LOGIC_VECTOR (0 to 9));
+    Port (  	clk   	: in STD_LOGIC;
+            	reset   : in STD_LOGIC;
+            	EN      : in STD_LOGIC;
+            	count   : out STD_LOGIC_VECTOR (0 to 9));
     end component;
 begin
-  r_osc <= Ring_oscilator;
+  	r_osc <= Ring_oscilator;
   
     clk_1 : clk_wiz_0 port map(
         clk_in1     => CLK100MHZ,
-        reset     => reset,
+        reset     	=> reset,
         clk_out     => clk,
         locked      => open
     );
